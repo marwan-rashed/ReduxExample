a019e890e553f553f22b9215cfe62ea4ab8abe83
Marwan Rashed <marwanrashed300@gmail.com>
2022-02-24 16:44:37 +0200

    Add navigation container

---
+++ src/screens/tasks.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {
+    View,
+} from 'react-native';
+
+export default Tasks = ({ navigation }) => {
+    return(
+        <View>
+            
+        </View>
+    );
+}
