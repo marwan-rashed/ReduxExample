47cd25ffc62a42b3cc46b4a61c4932f1bdcc2e81
Marwan Rashed <marwanrashed300@gmail.com>
2022-02-24 17:06:26 +0200

    Add navigation buttons

--- src/screens/tasks.js
+++ src/screens/tasks.js
@@ -1,12 +1,30 @@
 import React from "react";
 import {
     View,
+    Text,
+    TouchableOpacity,
 } from 'react-native';
 
 export default Tasks = ({ navigation }) => {
     return(
-        <View>
-            
+        <View style = {{
+            width: '100%', height: '100%',
+        }}>
+            {/* Add Float Button */}
+            <TouchableOpacity 
+                onPress = {() => navigation.navigate('Add Task')}
+                style = {{
+                    position: 'absolute', bottom: 30, right: 20,
+                    width: 60, height: 60, borderRadius: 30,
+                    backgroundColor: '#54AEFF',
+                    elevation: 5,
+                    justifyContent: 'center', alignItems: 'center',
+                }}
+            >
+                <Text style = {{
+                    color: 'white', fontSize: 35,
+                }}>+</Text>
+            </TouchableOpacity>
         </View>
     );
 }
